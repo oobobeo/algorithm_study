6c964021a34cef89d4a5521b63d13f10cdabdc66
송현근 <timxxz@naver.com>
2023-05-10 22:24:07 +0900

    no message

--- baekjoon/src/baekjoon/호텔.java
+++ baekjoon/src/baekjoon/호텔.java
@@ -1,10 +1,21 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class 호텔 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt();
+		int n = sc.nextInt();
+		
+		int[][] arr = new int[n][2];
+		for (int i = 0 ; i < n; i++) {
+			for (int j = 0; j < 2; j++) arr[i][j] = sc.nextInt();
+		}
+		
+		
 	}
 
 }
